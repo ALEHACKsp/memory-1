6b2ba7b0332000211df05585238e776312f4f14b
xo1337 <68725291+xo1337@users.noreply.github.com>
2021-05-17 23:47:06 -0600

    added more features + support for unicode charset

--- memory.h
+++ memory.h
@@ -1,4 +1,5 @@
 #pragma once
+#ifndef MEMORY_H
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <memory>
@@ -15,48 +16,51 @@ namespace Mem
 	}
 	namespace External {
 #if !_WIN32
-		std::uint32_t GetProcId32(const wchar_t* processName)
-		{
-			std::uint32_t ProcessId = NULL;
-			HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-			if (!(hSnap == NULL))
+	#if UNICODE
+			std::uint32_t GetProcId32(const wchar_t* processName)
 			{
-				PROCESSENTRY32 pe32;
-				pe32.dwSize = sizeof(pe32);
-				if (Process32First(hSnap, &pe32))
-					while (Process32Next(hSnap, &pe32))
-						if (wcscmp(processName, pe32.szExeFile))
-						{
-							ProcessId = pe32.th32ProcessID;
-							break;
-						}
+				std::uint32_t ProcessId = NULL;
+				HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+				if (!(hSnap == NULL))
+				{
+					PROCESSENTRY32 pe32;
+					pe32.dwSize = sizeof(pe32);
+					if (Process32First(hSnap, &pe32))
+						while (Process32Next(hSnap, &pe32))
+							if (wcscmp(processName, pe32.szExeFile))
+							{
+								ProcessId = pe32.th32ProcessID;
+								break;
+							}
 
+				}
+				CloseHandle(hSnap);
+				return ProcessId;
 			}
-			CloseHandle(hSnap);
-			return ProcessId;
-		}
 
-		std::uint32_t GetModuleBase32(std::uint32_t ProcessId, const wchar_t* moduleName)
-		{
-			std::uint32_t BaseAddress = NULL;
-			HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE32 | TH32CS_SNAPMODULE, ProcessId);
-			if (!(hSnap == NULL))
+			std::uint32_t GetModuleBase32(std::uint32_t ProcessId, const wchar_t* moduleName)
 			{
-				MODULEENTRY32 me32;
-				me32.dwSize = sizeof(me32);
-				if (Module32FirstW(hSnap, &me32))
-					while (Module32NextW(hSnap, &me32))
-						if (wcscmp(moduleName, me32.szModule))
-						{
-							BaseAddress = reinterpret_cast<std::uint32_t>(me32.modBaseAddr);
-							break;
-						}
-					
-			}
-			CloseHandle(hSnap);
-			return BaseAddress;
-		}
+				std::uint32_t BaseAddress = NULL;
+				HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE32 | TH32CS_SNAPMODULE, ProcessId);
+				if (!(hSnap == NULL))
+				{
+					MODULEENTRY32 me32;
+					me32.dwSize = sizeof(me32);
+					if (Module32FirstW(hSnap, &me32))
+						while (Module32NextW(hSnap, &me32))
+							if (wcscmp(moduleName, me32.szModule))
+							{
+								BaseAddress = reinterpret_cast<std::uint32_t>(me32.modBaseAddr);
+								break;
+							}
 
+				}
+				CloseHandle(hSnap);
+				return BaseAddress;
+			}
+	#else
+		//TODO, Add const char* support . . . lol . . .
+	#endif
 		std::uintptr_t GetPointerAddress(HANDLE ProcessHandle,
 			std::uintptr_t baseOfModule, std::vector<std::uintptr_t> pointers)
 		{
@@ -85,6 +89,8 @@ namespace Mem
 
 
 #else 
+
+#if !UNICODE
 		std::uint64_t GetProcId64(std::string process_name)
 		{
 			std::uint32_t ProcessId = NULL;
@@ -106,6 +112,7 @@ namespace Mem
 			return ProcessId;
 		}
 
+	
 		std::uint64_t GetModuleBase64(std::uint64_t ProcessId, const char* moduleName)
 		{
 			std::uint32_t BaseAddress = NULL;
@@ -125,35 +132,79 @@ namespace Mem
 			}
 			CloseHandle(hSnap);
 			return BaseAddress;
-		}
+		}	
+
+	
+	#else
+			std::uint64_t GetProcId64(const wchar_t* process_name)
+			{
+				std::uint32_t ProcessId = NULL;
+				HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+				if (!(hSnap == NULL))
+				{
+					PROCESSENTRY32W pe32;
+					pe32.dwSize = sizeof(pe32);
+					if (Process32FirstW(hSnap, &pe32))
+						while (Process32NextW(hSnap, &pe32))
+							if (!wcscmp(process_name, pe32.szExeFile))
+							{
+								ProcessId = pe32.th32ProcessID;
+								break;
+							}
+
+				}
+				CloseHandle(hSnap);
+				return ProcessId;
+			}
+
+			std::uint64_t GetModuleBase64(std::uint64_t ProcessId, const wchar_t* moduleName)
+			{
+				std::uint32_t BaseAddress = NULL;
+				HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE32 | TH32CS_SNAPMODULE, ProcessId);
+				if (!(hSnap == NULL))
+				{
+					MODULEENTRY32W me32;
+					me32.dwSize = sizeof(me32);
+					if (Module32FirstW(hSnap, &me32))
+						while (Module32NextW(hSnap, &me32))
+							if (wcscmp(moduleName, me32.szModule))
+							{
+								BaseAddress = reinterpret_cast<std::uint32_t>(me32.modBaseAddr);
+								break;
+							}
+
+				}
+				CloseHandle(hSnap);
+				return BaseAddress;
+			}
+	#endif
+
 #endif
 	}
 
 	namespace Internal
 	{
 		/*
-		
-			Inlining functions:
-			Remember, inlining is only a request to the compiler, 
-			not a command. Compiler can ignore the request for inlining. 
-			Compiler may not perform inlining in such circumstances like:
 
+			Inlining functions:
+			Remember, inlining is only a request to the compiler,
+			not a command. Compiler can ignore the request for inlining.
+			Compiler may not perform inlining in such circumstances like:
 			1) If a function contains a loop. (for, while, do-while)
 			2) If a function contains static variables.
 			3) If a function is recursive.
 			4) If a function return type is other than void, and the return statement doesn’t exist in function body.
 			5) If a function contains switch or goto statement.
-
 		*/
 
-		template<typename T> 
+		template<typename T>
 		inline void WPM(ULONG Address, T value)
 		{
 			try { *(T*)Address = value; }
 			catch (...) {}
 		}
 
-		template<typename T> 
+		template<typename T>
 		inline T RPM(ULONG Address)
 		{
 			try { return *(T*)Address; }
@@ -178,5 +229,12 @@ namespace Mem
 			return address;
 		}
 
+		PVOID GetUnhookedPointer(PVOID pointer)
+		{
+			if (*(unsigned char*)pointer == 0xF1)
+				return (PVOID)((ULONG)pointer + 0x1);
+		}
 	}
 }
+#define MEMORY_H
+#endif
